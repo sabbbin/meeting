fe7391c0015b64555153df37cc86dfef7da5775e
DESKTOP-I754IGB\Sushant Dangal <dangalsushant@gmail.com>
2022-08-15 17:53:03 +0545

    feat: children routes added to base layout

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,20 +2,33 @@ import { lazy } from "react";
 import { RouteObject } from "react-router-dom";
 
 
+
 const Login = lazy(() => import('./pages/login'));
 const Dashboard = lazy(() => import('./pages/dashboard'));
+const BaseLayout = lazy(() => import('./pages/baseLayout'));
+const User = lazy(() => import('./pages/user'));
 
 
 
 export const AppRoutes: RouteObject[] = [
 
     {
-        path: '/',
+        path: '/login',
         element: <Login />
     },
     {
-        path: '/dashboard',
-        element: <Dashboard />
-    }
+        path: '/',
+        element: <BaseLayout />,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+            {
+                path: '/userTable',
+                element: < User />
+            },
+        ]
+    },
 
 ]
