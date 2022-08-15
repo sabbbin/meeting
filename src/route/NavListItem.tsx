a3ee9e478c095b155202d7ef50b12a0c5c76dbde
DESKTOP-I754IGB\Sushant Dangal <dangalsushant@gmail.com>
2022-08-15 17:54:31 +0545

    feat: navlist route component created

---
+++ src/route/NavListItem.tsx
@@ -0,0 +1,54 @@
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { Collapse, Divider, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { ReactNode, useState } from "react";
+import { Roles } from "../pages/roles";
+
+
+interface INavListProps {
+    items: INavItemsProps[];
+}
+
+const Navlist = ({ items }: INavListProps) => {
+
+    return (
+        <List>
+            {items.map(
+                (item, index) =>
+                    <NavListItems {...item} key={index} />
+            )}
+        </List>
+    );
+};
+
+export interface INavItemsProps {
+    label: string;
+    icon: ReactNode;
+    onClick?: () => void;
+    role?: Roles[];
+    items?: INavItemsProps[];
+}
+const NavListItems = ({ label, icon, onClick, items }: INavItemsProps) => {
+    const [open, setOpen] = useState(false);
+
+    const handleChange = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <>
+            <ListItem button onClick={items ? handleChange : onClick}>
+                <ListItemIcon>{icon}</ListItemIcon>
+                <ListItemText primary={label} />
+                {items && (open ? <ExpandLess /> : <ExpandMore />)}
+            </ListItem>
+            {items && (
+                <Collapse in={open}>
+                    <Navlist items={items} />
+                    <Divider />
+                </Collapse>
+            )}
+        </>
+    );
+};
+
+export default Navlist;
