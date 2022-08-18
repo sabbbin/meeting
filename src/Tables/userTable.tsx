bee6d76e501537200904bfc3ad41bd4b99cbdf4a
DESKTOP-I754IGB\Sushant Dangal <dangalsushant@gmail.com>
2022-08-18 15:24:41 +0545

    feat: added yup validation added

--- src/Tables/userTable.tsx
+++ src/Tables/userTable.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Fab, IconButton, Menu, MenuItem, MenuList, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Toolbar } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Fab, IconButton, Menu, MenuItem, MenuList, Paper, PaperTypeMap, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Toolbar } from "@mui/material";
 import {
     createColumnHelper,
     flexRender,
@@ -6,13 +6,16 @@ import {
     useReactTable,
 } from '@tanstack/react-table';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { MouseEvent, useMemo, useState } from "react";
+import { memo, MouseEvent, useMemo, useState } from "react";
 import useUsers from "../hooks/useUsers";
 import usePagination from "@mui/material/usePagination/usePagination";
 import { Add } from "@mui/icons-material";
+import { Field, Form, Formik, FormikHelpers, useFormik } from "formik";
+import * as yup from 'yup';
+import { OverridableComponent } from "@mui/material/OverridableComponent";
 
 export interface IUser {
-    username: string,
+    userName: string,
     fullName: string,
     email: string,
     role: string,
@@ -21,46 +24,87 @@ export interface IUser {
     createdOn: string
 };
 
+
+interface Values {
+    userName: string,
+    email: string,
+    fullName: string,
+    password: string,
+    confirmPassword: string,
+}
+
+const validationSchema = yup.object({
+    userName: yup
+        .string()
+        .required('Username is required')
+        .typeError('Username name must be a string'),
+    email: yup
+        .string()
+        .email('Enter a valid email')
+        .required('Email is required')
+        .typeError('Enter a valid email'),
+    fullName: yup
+        .string()
+        .required('Please enter your Full')
+        .typeError('Fullname name must be a string'),
+    password: yup
+        .string()
+        .min(8, 'Password should be of minimum 8 characters length')
+        .required('Password is required')
+        .typeError('Password name must be a string'),
+    confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password'), null], 'Password and confirm password should match')
+        .min(8, 'Password should be of minimum 8 characters length')
+        .required('Password is required')
+        .typeError('Password and confirm password should match'),
+});
+
+
+
 const columnHelper = createColumnHelper<IUser>()
 
-const defaultData: IUser[] = [
-    {
-        username: 'Jadhav',
-        fullName: 'jyaa dyab bro',
-        email: 'dangalsushant@gmail.com',
-        status: 'Active',
-        createdBy: 'raj',
-        createdOn: 'april 4 2022',
-        role: 'admin',
-    },
-    {
-        username: 'yadav',
-        fullName: 'wyadov  bro',
-        email: 'yadav@gmail.com',
-        status: 'Active',
-        createdBy: 'yaj',
-        createdOn: 'april 3 2022',
-        role: 'admin',
-    },
-    {
-        username: 'madhav',
-        email: 'madhav@gmail.com',
-        fullName: 'maya dov  bro',
-        status: 'Active',
-        createdBy: 'myaj',
-        createdOn: 'april 2 2022',
-        role: 'admin',
-    },
-    {
-        username: 'padhav',
-        email: 'padhav@gmail.com',
-        fullName: 'paya dov bro',
-        status: 'Active',
-        createdBy: 'pyaj',
-        createdOn: 'april 1 2022',
-        role: 'admin',
-    }
-]
+// const defaultData: IUser[] = [
+//     {
+//         username: 'Jadhav',
+//         fullName: 'jyaa dyab bro',
+//         email: 'dangalsushant@gmail.com',
+//         status: 'Active',
+//         createdBy: 'raj',
+//         createdOn: 'april 4 2022',
+//         role: 'admin',
+//     },
+//     {
+//         username: 'yadav',
+//         fullName: 'wyadov  bro',
+//         email: 'yadav@gmail.com',
+//         status: 'Active',
+//         createdBy: 'yaj',
+//         createdOn: 'april 3 2022',
+//         role: 'admin',
+//     },
+//     {
+//         username: 'madhav',
+//         email: 'madhav@gmail.com',
+//         fullName: 'maya dov  bro',
+//         status: 'Active',
+//         createdBy: 'myaj',
+//         createdOn: 'april 2 2022',
+//         role: 'admin',
+//     },
+//     {
+//         username: 'padhav',
+//         email: 'padhav@gmail.com',
+//         fullName: 'paya dov bro',
+//         status: 'Active',
+//         createdBy: 'pyaj',
+//         createdOn: 'april 1 2022',
+//         role: 'admin',
+//     }
+
+// ]
+
+const FormDialogPaper = (props: OverridableComponent<PaperTypeMap<{}, "div">>) => <Paper {...props as any} as="form" />
 
 
 export default function UserTable() {
@@ -88,7 +132,7 @@ export default function UserTable() {
 
     const columns = useMemo(() =>
         [
-            columnHelper.accessor('username', {
+            columnHelper.accessor('userName', {
                 header: "Username",
                 cell: info => info.getValue(),
                 footer: info => info.column.id,
@@ -112,12 +156,12 @@ export default function UserTable() {
             columnHelper.accessor(row => row.status, {
                 header: "Status",
                 cell: info => info.getValue(),
-                footer: info => info.column.id,
             }),
             columnHelper.accessor(row => row.createdBy, {
                 header: "Created By",
                 cell: info => info.getValue(),
                 footer: info => info.column.id,
+
             }),
             columnHelper.accessor(row => row.createdOn, {
                 header: "Created On",
@@ -135,30 +179,59 @@ export default function UserTable() {
         ([]))
 
 
-    const { data } = useUsers();
+    const { data } = useUsers({
+        headers: {
+            Authorization: `Bearer eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9naXZlbm5hbWUiOiJOaXIgS3IuIFJhaSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoibmlya3JhaSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL2VtYWlsYWRkcmVzcyI6Im5pci5yYWlAY2hhbm5ha3lhc29mdC5jb20iLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiIzIiwiZXhwIjoxNjYwODE3NDQ4LCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MjgzIiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NzI4MyJ9.78ethsXk_Ehz7CwzGaCxIZEEyTfjsPKHjwohPQ6custRX0beT-jw2-hpaXI7rvIBGkCeAezWQpTklqcYMMc-WA`
+        }
+    });
 
     const table = useReactTable({
-        data: defaultData,
+        data,
         columns,
         getCoreRowModel: getCoreRowModel(),
 
     });
 
+    const formik = useFormik({
+        initialValues: {
+            userName: '',
+            email: '',
+            fullName: '',
+            password: '',
+            confirmPassword: ''
+        },
+        validationSchema: validationSchema,
+        onSubmit:
+            (values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 500);
+            }
+    })
+
     return (
         <>
             <Toolbar />
-            <Button variant="contained" onClick={handleClickOpen}>
+            <Button sx={{ m: 1 }} variant="contained" onClick={handleClickOpen}>
                 Add User
             </Button>
-            <Dialog open={open} onClose={handleClose}>
+            <Dialog PaperComponent={FormDialogPaper as never} PaperProps={{
+                onSubmit: formik.handleSubmit as never
+            }} open={open} onClose={handleClose}>
                 <DialogTitle>Add user to Channakya-Meetings</DialogTitle>
                 <DialogContent>
                     <TextField
                         autoFocus
                         margin="dense"
                         id="name"
+                        name="userName"
+                        value={formik.values.userName}
+                        onChange={formik.handleChange}
+                        error={formik.touched.userName && Boolean(formik.errors.userName)}
+                        helperText={formik.touched.userName && formik.errors.userName}
                         label="Username"
-                        type="Username"
+                        type="name"
                         fullWidth
                         variant="standard"
                     />
@@ -166,6 +239,11 @@ export default function UserTable() {
                         autoFocus
                         margin="dense"
                         id="name"
+                        name="email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
                         label="Email Address"
                         type="email"
                         fullWidth
@@ -175,6 +253,11 @@ export default function UserTable() {
                         autoFocus
                         margin="dense"
                         id="name"
+                        name="fullName"
+                        value={formik.values.fullName}
+                        onChange={formik.handleChange}
+                        error={formik.touched.fullName && Boolean(formik.errors.fullName)}
+                        helperText={formik.touched.fullName && formik.errors.fullName}
                         label="Full Name"
                         type="name"
                         fullWidth
@@ -184,6 +267,11 @@ export default function UserTable() {
                         autoFocus
                         margin="dense"
                         id="Password"
+                        name="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
                         label="Password"
                         type="Password"
                         fullWidth
@@ -193,16 +281,20 @@ export default function UserTable() {
                         autoFocus
                         margin="dense"
                         id="Password"
+                        name="confirmPassword"
+                        value={formik.values.confirmPassword}
+                        onChange={formik.handleChange}
+                        error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+                        helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
                         label="Confirm Password"
                         type="Password"
                         fullWidth
                         variant="standard"
                     />
-
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleClose}>Add user</Button>
+                    <Button type="submit">Add user</Button>
                 </DialogActions>
             </Dialog>
             <TableContainer sx={{ minWidth: 1000, margin: '1' }} component={Paper} >
--- src/hooks/useUsers.tsx
+++ src/hooks/useUsers.tsx
@@ -1,13 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
-function useUsers() {
+function useUsers(axiosConfig: AxiosRequestConfig) {
     return useQuery(
         ["posts"],
         async () => await axios.get(
-            "http://110.34.24.117:90/api/User/GetAllUser/10/1"
+            "http://110.34.24.117:90/api/User/GetAllUser/10/1",
+            axiosConfig
         ).then((res) => res.data)
-
+        , { initialData: [] }
     )
 }
 
