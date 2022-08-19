49ea5f79256ca8b27029e31813eebb36022d285d
DESKTOP-I754IGB\Sushant Dangal <dangalsushant@gmail.com>
2022-08-19 17:27:32 +0545

    feat: user data added to usertable

--- src/hooks/useUsers.tsx
+++ src/hooks/useUsers.tsx
@@ -5,7 +5,7 @@ function useUsers(axiosConfig: AxiosRequestConfig) {
     return useQuery(
         ["posts"],
         async () => await axios.get(
-            "http://110.34.24.117:90/api/User/GetAllUser/10/1",
+            "api/User/GetAllUser/3/1",
             axiosConfig
         ).then((res) => res.data)
         , { initialData: [] }
