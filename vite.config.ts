adfd714d7233f89e9b48af7a6c817d8d565956c9
DESKTOP-I754IGB\Sushant Dangal <dangalsushant@gmail.com>
2022-08-19 17:27:51 +0545

    feat: added a proxy server

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: 'http://110.34.24.117:90',
+        changeOrigin: true
+      }
+    }
+  }
 })
