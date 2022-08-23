2c4321e7818c345dadb708000163afb0833ec94d
DESKTOP-I754IGB\Sushant Dangal <dangalsushant@gmail.com>
2022-08-23 10:25:30 +0545

    feat: cssbaseline added

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { CssBaseline } from '@mui/material'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -9,8 +10,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
 
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
         <App />
       </QueryClientProvider>
     </BrowserRouter>
