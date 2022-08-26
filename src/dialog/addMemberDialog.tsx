c2e01c4eb26354d9bfe4291da538fb4d30fa11c3
DESKTOP-I754IGB\Sushant Dangal <dangalsushant@gmail.com>
2022-08-26 15:02:44 +0545

    feat: dailog created to addmember

---
+++ src/dialog/addMemberDialog.tsx
@@ -0,0 +1,95 @@
+import { Button, Chip, Dialog, DialogActions, DialogContent, DialogContentText, DialogProps, DialogTitle, Fab, IconButton, Menu, MenuItem, MenuList, Paper, PaperTypeMap, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, TextField, Toolbar } from "@mui/material";
+
+import * as React from 'react';
+import { Theme, useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+
+
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const names = [
+    'Enable',
+    'Suspended',
+    'Disable',
+    'Deleted',
+];
+
+interface AddMemberDialogProps extends DialogProps {
+    onDiscardAddMemberDialog: () => void;
+    onSuccessAddMemberDialog: () => void;
+}
+
+
+export default function AddMemberDialog({ onDiscardAddMemberDialog, onSuccessAddMemberDialog, open }: AddMemberDialogProps) {
+
+    const [personName, setPersonName] = React.useState<string[]>([])
+
+    const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+        const {
+            target: { value },
+        } = event;
+        setPersonName(
+            // On autofill we get a stringified value.
+            typeof value === 'string' ? value.split(',') : value,
+        );
+    };
+
+    const handleClose = () => {
+        onDiscardAddMemberDialog()
+
+    };
+    return (
+        <>
+            <Dialog open={open} onClose={handleClose} sx={{ m: 1, width: 300 }}>
+                <DialogTitle>Add/remove user from Meetings</DialogTitle>
+                <DialogContent>
+                    <Select
+                        fullWidth
+                        labelId="demo-multiple-chip-label"
+                        id="demo-multiple-chip"
+                        multiple
+                        value={personName}
+                        onChange={handleChange}
+                        input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                        renderValue={(selected) => (
+                            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                                {selected.map((value) => (
+                                    <Chip key={value} label={value} />
+                                ))}
+                            </Box>
+                        )}
+                        MenuProps={MenuProps}
+                    >
+                        {names.map((name) => (
+                            <MenuItem
+                                key={name}
+                                value={name}>
+                                {name}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onDiscardAddMemberDialog}>Cancel</Button>
+                    <Button type="submit">Submit</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    )
+}
+
