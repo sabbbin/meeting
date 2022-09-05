c2ab17385d84e534dcf2a44f604e9f2bec4b710a
sabbbin <suwal721063@gmail.com>
2022-09-05 13:12:35 +0545

    fix:  fixing error in change status in user

--- src/hooks/useStatus.tsx
+++ src/hooks/useStatus.tsx
@@ -2,23 +2,21 @@ import { useQuery } from "@tanstack/react-query";
 import axios, { AxiosRequestConfig } from "axios";
 
 interface UserStatus {
-    StatusId: number,
-    StatusName: string,
-    Description: string,
-    OrderIdx: number
+  StatusId: number;
+  StatusName: string;
+  Description: string;
+  OrderIdx: number;
 }
 
 function useStatus(axiosConfig: AxiosRequestConfig) {
-    return useQuery<UserStatus[]>(
-        ["userStatus"],
-        async () => await axios.get(
-            "api/User/GetUserStatus",
-            axiosConfig
-        ).then((res) => res.data), {
-        initialData: []
+  return useQuery<UserStatus[]>(
+    ["userStatus"],
+    () =>
+      axios.get("api/User/GetUserStatus", axiosConfig).then((res) => res.data),
+    {
+      initialData: [],
     }
-
-    )
+  );
 }
 
 export default useStatus;
